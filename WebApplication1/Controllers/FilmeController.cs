b0702506c4f8376451e201f27289f8503fe578f9
Carlos Pivotto <carlospivotto@msn.com>
2020-08-26 17:25:02 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Controllers/FilmeController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class FilmeController : Controller
+    {
+        // GET: FilmeController
+        public ActionResult Index()
+        {
+            var filmes = new List<Filme>();
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=InfnetFilmes;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using var connection = new SqlConnection(connectionString);
+            var sp = "ListarFilmes";
+            var sqlCommand = new SqlCommand(sp, connection);
+
+            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            try
+            {
+                connection.Open();
+                using var reader = sqlCommand.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                while (reader.Read())
+                {
+                    var filme = new Filme
+                    {
+                        FilmeId = (int)reader["FilmeId"],
+                        Titulo = reader["Titulo"].ToString(),
+                        TituloOriginal = reader["TituloOriginal"].ToString()
+                    };
+
+                    filmes.Add(filme);
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } finally
+            {
+                connection.Close();
+            }
+
+            return View(filmes);
+        }
+
+        // GET: FilmeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FilmeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FilmeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FilmeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FilmeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FilmeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FilmeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Filme
+    {
+        public int FilmeId { get; set; }
+        public string Titulo { get; set; }
+        public string TituloOriginal { get; set; }
+        public int AnoLancamento { get; set; }
+        public string ClassificacaoEtaria { get; set; }
+        public int Duracao { get; set; }
+    }
+}
